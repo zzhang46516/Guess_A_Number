1917d3e54b882d3fbb55e196bae79743bda831cb
Zhenwang <zhenwang@lis-macbook-pro.attlocal.net>
2018-02-05 21:11:59 -0600

    first commit

---
+++ Guess_A_Number.py
@@ -0,0 +1,20 @@
+import random
+my_random_number = random.randint(1, 10)
+print("I am thinking of a number form 1 to 10")
+guesses = 0
+while guesses < 5:
+    print("You have", 5 - guesses, "guesses left")
+    guesses += 1
+    raw_input = input("What's the number?")
+    guess_number = int(raw_input)
+    print(guess_number)
+    if guess_number == my_random_number:
+        print("Yes! You win!")
+        break
+    else:
+        if guess_number < my_random_number:
+            print(guess_number, "is too low.")
+        else:
+            print(guess_number, "is too high.")
+    if guesses == 5:
+        print("You are out of guesses")
